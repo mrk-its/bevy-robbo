930fa4286d32f866e4946fe1625e0b39ae901736
Mariusz Kryński <mrk@sed.pl>
2020-09-13 19:46:30 +0200

    improve magnetic field system

--- src/systems/magnetic_field.rs
+++ src/systems/magnetic_field.rs
@@ -1,13 +1,14 @@
-use crate::components::{Int2Ops, Magnet, MovingDir, Position, Robbo};
+use crate::components::{Int2Ops, Magnet, MovingDir, Position, Robbo, Wall};
+use crate::levels::LevelInfo;
 use bevy::prelude::*;
-use std::collections::HashSet;
 
 pub fn magnetic_field_system(
+    level_info: Res<LevelInfo>,
     mut robbo: Query<With<Robbo, (&mut Position, &mut MovingDir)>>,
-    mut all: Query<&Position>,
+    mut all: Query<Without<Wall, (&Position, Entity)>>,
     mut magnets_query: Query<(&Magnet, &Position)>,
 ) {
-    let occupied: HashSet<Position> = all.iter().iter().cloned().collect();
+    let occupancy = level_info.get_occupancy(&mut all);
     for (robbo_pos, mut moving_dir) in &mut robbo.iter() {
         let attracts = |dir: i32, a: i32, b: i32| dir == (b - a).signum();
         for (magnet_dir, magnet_pos) in magnets_query.iter().iter().filter(|(magnet, pos)| {
@@ -15,7 +16,7 @@ pub fn magnetic_field_system(
                 || (pos.y() == robbo_pos.y()) && attracts(magnet.x(), pos.x(), robbo_pos.x())
         }) {
             let mut pos = magnet_pos.add(magnet_dir);
-            while pos != *robbo_pos && !occupied.contains(&pos) {
+            while pos != *robbo_pos && occupancy.is_free(&pos) {
                 pos = pos.add(magnet_dir);
             }
             if pos == *robbo_pos {
