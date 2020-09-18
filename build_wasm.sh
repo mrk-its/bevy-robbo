26ed3e1e3c40741838d719ec464b09c42f3a5a30
Mariusz Kryński <mrk@sed.pl>
2020-09-18 21:52:14 +0200

    fix styles / ascii tiles

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,3 +1,3 @@
 RUST_LOG=info cargo build --target wasm32-unknown-unknown --release --no-default-features
-wasm-bindgen --out-dir wasm/target --target web ${CARGO_TARGET_DIR:-target}/wasm32-unknown-unknown/release/bevy-robbo.wasm
+wasm-bindgen --out-name bevy-robbo-v3 --out-dir wasm/target --target web ${CARGO_TARGET_DIR:-target}/wasm32-unknown-unknown/release/bevy-robbo.wasm
 # basic-http-server wasm
