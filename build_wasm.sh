1458bf9b8fc8906ed6a6d1476101c03d329b7b47
Mariusz Kryński <mrk@sed.pl>
2020-10-20 10:09:17 +0200

    add ga, add --no-typescript to build script

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,3 +1,3 @@
 RUST_LOG=info cargo build --release --target wasm32-unknown-unknown --no-default-features --features bevy/bevy_webgl2
-wasm-bindgen --out-name bevy-robbo --out-dir assets/wasm/target --target web ${CARGO_TARGET_DIR:-target}/wasm32-unknown-unknown/release/bevy-robbo.wasm
+wasm-bindgen --no-typescript --out-name bevy-robbo --out-dir assets/wasm/target --target web ${CARGO_TARGET_DIR:-target}/wasm32-unknown-unknown/release/bevy-robbo.wasm
 basic-http-server
